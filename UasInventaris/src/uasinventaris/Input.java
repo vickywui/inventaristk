17199889ab19893bb758e11a9e56c66e20d7126c
vickywui <54465437+vickywui@users.noreply.github.com>
2021-06-15 19:59:20 +0700

    Update Input.java

--- UasInventaris/src/uasinventaris/Input.java
+++ UasInventaris/src/uasinventaris/Input.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */ 
-package uasinventaris;
+package ujianinventaris;
 
 import java.awt.HeadlessException;
 import java.sql.Connection;
